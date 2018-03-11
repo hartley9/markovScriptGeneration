66f488b0faa386f377a3ec54ddb8fd6248733066
hartley9 <k.hartley1@lancaster.ac.uk>
2018-03-11 21:34:00 +0000

    scroll fixed

--- sketch.js
+++ sketch.js
@@ -1,9 +1,12 @@
-var input, inp, submit, clearStuff, markov, data, out, x=160, y=240;
+var myCanvas, input, submit, clearStuff, ngrams, sentenceNumber, markov, out;
+var x=160, y=240;
 
 function setup() {
 	//createCanvas(windowWidth, windowHeight);
-	noCanvas();
-	background(250, 255, 200);
+	myCanvas = createCanvas(windowWidth, windowHeight);
+	myCanvas.parent('container');
+
+
 	textFont('times', 18);
 
 	textInput = select("#textfield")
@@ -11,8 +14,8 @@ function setup() {
 	submit = select("#submit");
 	submit.mousePressed(markovRita);
 
-	clearStuff = select("#clear");
-	clearStuff.mousePressed(clearCanvas);
+	//clearStuff = select("#clear");
+	//clearStuff.mousePressed(clearCanvas)
 
 	/*
 	subBut = createButton('submit');
@@ -29,7 +32,10 @@ function setup() {
 
 function drawText() {
 	//text(out, x, y, windowWidth/2, windowHeight/2);
-	createP(out);
+	console.log(ngrams);
+	var elt = document.getElementById('output');
+	var myP = createP(out);
+	myP.parent(elt);
 }
 
 function clearCanvas(){
@@ -40,14 +46,20 @@ function clearCanvas(){
 
 function markovRita() {
 
+
+	sentenceNumber = select("#sentNumber");
+	sentenceNumber = parseInt(sentenceNumber.value());
+	ngrams = select("#ngrams");
+	ngrams = parseInt(ngrams.value());
+
 	//data = document.getElementById("form1");
 	//input = data.elements["input"].value;
 
-	markov = new RiMarkov(2);
+	markov = new RiMarkov(ngrams);
 
 	markov.loadText(textInput.value());
 
-	out = markov.generateSentences(5);
+	out = markov.generateSentences(sentenceNumber);
 	console.log(out);
 
 	drawText();
