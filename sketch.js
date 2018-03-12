71d32b62cdef2505b9781fed35f50089b9192394
hartley9 <k.hartley1@lancaster.ac.uk>
2018-03-12 18:04:13 +0000

    pingu

--- sketch.js
+++ sketch.js
@@ -1,10 +1,19 @@
-var myCanvas, input, submit, clearStuff, ngrams, sentenceNumber, markov, out;
+var myCanvas;
+var submit, clearStuff;
+var ngrams, sentenceNumber, markov, out;
+var imgPingu, imgJosh
 var x=160, y=240;
 
 function setup() {
 	//createCanvas(windowWidth, windowHeight);
 	myCanvas = createCanvas(windowWidth, windowHeight);
 	myCanvas.parent('container');
+
+
+	imgPingu = loadImage("pingu.jpg");
+	imgJosh = loadImage("josh.jpg");
+
+	//img.parent('container');
 
 
 	textFont('times', 18);
@@ -14,18 +23,23 @@ function setup() {
 	submit = select("#submit");
 	submit.mousePressed(markovRita);
 
-	//clearStuff = select("#clear");
-	//clearStuff.mousePressed(clearCanvas)
+}
 
-	/*
-	subBut = createButton('submit');
-  subBut.position(x, y);
-  subBut.mousePressed(markovRita);
+function draw()
+{
 
-	clearBut = createButton('clear');
-	clearBut.position(x+subBut.width, y);
-	clearBut.mousePressed(clearCanvas);
-	*/
+}
+
+function mouseWheel(event)
+{
+	var randX = random(0, windowWidth);
+	var randY = random(0, windowHeight);
+
+	var ping = image(imgPingu, randX, randY, imgPingu.width/2, imgPingu.height/2);
+	ping.parent('#header');
+
+	var josh = image (imgJosh, randX, randY, imgJosh.width/2, imgJosh.height/2);
+	josh.parent('#input');
 
 }
 
